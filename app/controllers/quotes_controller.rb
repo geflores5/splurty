3761dd959304efb45004537aaa14f055dd98b41c
Giovanni Flores <geflores5@gmailcom>
2017-02-03 06:32:52 +0000

    Added input validation

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,8 +8,13 @@ class QuotesController < ApplicationController
   end
 
   def create
-    Quote.create(quote_params)
-    redirect_to root_path
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered in invalid.'
+      redirect_to new_quote_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
