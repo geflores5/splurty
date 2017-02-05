dc1d6172888d899adc75a82f80eda78f4496ba48
Giovanni Flores <geflores5@gmailcom>
2017-02-05 06:38:23 +0000

    Removed Yoda Test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,9 +1,4 @@
 class Quote < ApplicationRecord
   validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
-  validates :author, presence: true, if: :mustbeYoda
-  def mustbeYoda
-    if :author != "Yoda"
-      errors.add(:author)
-    end
-  end
+  validates :author, presence: true, length: { maximum: 4, minimum: 4 }
 end
